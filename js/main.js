5a5d19a417af9ddefd96f5a65d028d201e44e892
lutfiagustamel <lutfyagustamel.la@gmail.com>
2023-05-29 20:31:25 +0700

    add parallax effect

--- js/main.js
+++ js/main.js
@@ -1,53 +1,60 @@
-function input(input){
-    let angka = document.getElementById("screen2").innerHTML;
-    angka = angka+input;
-    document.getElementById("screen2").innerHTML=angka;
-    if (input == "+"||input == "-"||input == "*"||input == "/"){
-        let angka2 = document.getElementById("screen").innerHTML;
-        angka2=angka2+angka;
-        document.getElementById("screen").innerHTML=angka2;
-        document.getElementById("screen2").innerHTML="";
+let result = document.getElementById('result');
+let operatorClicked = false;
+let currentTheme = 'light-theme';
+
+function appendNumber(number) {
+    result.innerHTML += number;
+    operatorClicked = false;
+}
+
+function appendOperator(operator) {
+    if (!operatorClicked) {
+        result.innerHTML += operator;
+        operatorClicked = true;
     }
 }
 
-function hasil(){
-    let angka = document.getElementById("screen2").innerHTML;
-    let angka2 = document.getElementById("screen").innerHTML;
-    angka2=angka2+angka;
-    document.getElementById("screen").innerHTML=angka2;
-    document.getElementById("screen2").innerHTML="";
-    let hasil = eval(angka2);
-    document.getElementById("screen2").innerHTML="="+hasil;
+function clearResult() {
+    result.innerHTML = 0;
+    operatorClicked = false;
 }
 
-function reset(){
-    document.getElementById("screen").innerHTML="";
-    document.getElementById("screen2").innerHTML="";
-}
-
-function persen(){
-    let angka = document.getElementById("screen2").innerHTML;
-    let persen = angka/100;
-    document.getElementById("screen2").innerHTML=persen;
-}
-
-function hapus(){
-    let angka = document.getElementById("screen2").innerHTML;
-    let hapus = angka.slice(0,-1);
-    document.getElementById("screen2").innerHTML=hapus;
-}
-
-function min(){
-    let angka = document.getElementById("screen2").innerHTML;
-    if (angka.includes("(")){
-        let a = angka.slice(1,-1);
-        let plus = Math.abs(a);
-        document.getElementById("screen2").innerHTML=plus;
+function calculate() {
+    try {
+        const expression = result.innerHTML;
+        const answer = eval(expression);
+        result.innerHTML = answer;
+        operatorClicked = false;
+    } catch (error) {
+        result.innerHTML = 'Error';
     }
-    
-    if (angka>0){
-        let min = -Math.abs(angka);
-        angka=document.getElementById("screen2").innerHTML="("+min+")";
-        document.getElementById("screen2").innerHTML=angka;
+}
+
+function toggleTheme() {
+    const body = document.body;
+    const container = document.querySelector('.container');
+    const result = document.querySelector('.result');
+    const buttons = document.querySelectorAll('.button');
+    const themeButton = document.querySelector('.theme-button');
+
+    body.classList.remove(currentTheme);
+    container.classList.remove(currentTheme);
+    result.classList.remove(currentTheme);
+    buttons.forEach(button => button.classList.remove(currentTheme));
+
+    if (currentTheme === 'light-theme') {
+        currentTheme = 'black-theme';
+        themeButton.innerText = 'Toggle Black Theme';
+    } else if (currentTheme === 'Black-theme') {
+        currentTheme = 'light-theme';
+        themeButton.innerText = 'Toggle Light Theme';
+    } else {
+        currentTheme = 'light-theme';
+        themeButton.innerText = 'Toggle Light Theme';
     }
+
+    body.classList.add(currentTheme);
+    container.classList.add(currentTheme);
+    result.classList.add(currentTheme);
+    buttons.forEach(button => button.classList.add(currentTheme));
 }
